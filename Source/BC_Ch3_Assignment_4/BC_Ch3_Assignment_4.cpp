e1225c95d7dfffd8277e8cbd22085e091b93b12c
DevCol <devcol.main@gmail.com>
2026-06-05 11:50:41 +0900

    Initial commit

---
+++ Source/BC_Ch3_Assignment_4/BC_Ch3_Assignment_4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BC_Ch3_Assignment_4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BC_Ch3_Assignment_4, "BC_Ch3_Assignment_4" );
